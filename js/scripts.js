cf38f3b53d68d82755ed7162fb0a9352a378da1d
rpcedill <rpcedill@espol.edu.ec>
2021-11-17 17:50:03 -0500

    globo

--- js/scripts.js
+++ js/scripts.js
@@ -1,54 +1,102 @@
 /*!
-* Start Bootstrap - Freelancer v7.0.5 (https://startbootstrap.com/theme/freelancer)
+* Start Bootstrap - Clean Blog v6.0.7 (https://startbootstrap.com/theme/clean-blog)
 * Copyright 2013-2021 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-freelancer/blob/master/LICENSE)
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-clean-blog/blob/master/LICENSE)
 */
-//
-// Scripts
-// 
-
-window.addEventListener('DOMContentLoaded', event => {
-
-    // Navbar shrink function
-    var navbarShrink = function () {
-        const navbarCollapsible = document.body.querySelector('#mainNav');
-        if (!navbarCollapsible) {
-            return;
-        }
-        if (window.scrollY === 0) {
-            navbarCollapsible.classList.remove('navbar-shrink')
-        } else {
-            navbarCollapsible.classList.add('navbar-shrink')
-        }
-
-    };
-
-    // Shrink the navbar 
-    navbarShrink();
-
-    // Shrink the navbar when page is scrolled
-    document.addEventListener('scroll', navbarShrink);
-
-    // Activate Bootstrap scrollspy on the main nav element
-    const mainNav = document.body.querySelector('#mainNav');
-    if (mainNav) {
-        new bootstrap.ScrollSpy(document.body, {
-            target: '#mainNav',
-            offset: 72,
-        });
-    };
-
-    // Collapse responsive navbar when toggler is visible
-    const navbarToggler = document.body.querySelector('.navbar-toggler');
-    const responsiveNavItems = [].slice.call(
-        document.querySelectorAll('#navbarResponsive .nav-link')
-    );
-    responsiveNavItems.map(function (responsiveNavItem) {
-        responsiveNavItem.addEventListener('click', () => {
-            if (window.getComputedStyle(navbarToggler).display !== 'none') {
-                navbarToggler.click();
+window.addEventListener('DOMContentLoaded', () => {
+    let scrollPos = 0;
+    const mainNav = document.getElementById('mainNav');
+    const headerHeight = mainNav.clientHeight;
+    window.addEventListener('scroll', function() {
+        const currentTop = document.body.getBoundingClientRect().top * -1;
+        if ( currentTop < scrollPos) {
+            // Scrolling Up
+            if (currentTop > 0 && mainNav.classList.contains('is-fixed')) {
+                mainNav.classList.add('is-visible');
+            } else {
+                console.log(123);
+                mainNav.classList.remove('is-visible', 'is-fixed');
             }
-        });
+        } else {
+            // Scrolling Down
+            mainNav.classList.remove(['is-visible']);
+            if (currentTop > headerHeight && !mainNav.classList.contains('is-fixed')) {
+                mainNav.classList.add('is-fixed');
+            }
+        }
+        scrollPos = currentTop;
     });
+})
 
-});
+function initialize() {
+    var earth = new WE.map('earth_div');
+    WE.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(earth);
+
+    fetch("composer.json")
+    .then(response => response.json())
+    .then(data => {
+        for(let i = 0; i < Object.keys(data).length ; i++){
+            let htmLine = "<b>"+ data[i]['name'] + "</b><br>Artistas más escuchados<br><a href='https://spotifycharts.com/regional/"+ data[i]['country'].toLowerCase()+"/daily/latest'>click here</a>";
+            let latitude = data[i]['latitude'];
+            let longitude = data[i]['longitude'];
+            var marker = WE.marker([latitude, longitude]).addTo(earth);
+            marker.bindPopup(htmLine, {maxWidth: 120, closeButton: true});
+        }
+    })
+    .catch(console.error);
+
+    var markerCustom = WE.marker([50, -9], '/img/logo-webglearth-white-100.png', 100, 24).addTo(earth);
+
+    earth.setView([-1.6, -78], 4);
+}
+
+
+const peticion = () => {
+   let proxy = 'https://damp-beach-17296.herokuapp.com/'
+   let url = 'https://api.eluniverso.arcpublishing.com/feeds/rss/?website=el-universo&query=taxonomy.sections._id:%22/entretenimiento/musica%22&sort=first_publish_date:desc'
+
+   fetch(proxy+url)
+   .then(response => response.text())
+   .then(data =>{
+     const parser = new DOMParser();
+     const xml = parser.parseFromString(data, "application/xml");
+
+     let items =xml.getElementsByTagName('item')
+
+     for(let i=0 ; i < 5 ; i++){
+       let plantilla = `
+        <div class="card mb-3" >
+                <div class="row g-0">
+                  <div class="col-md-4">
+                    <img src="assets/img/news01.jpg" class="img-fluid img-thumbnail" alt="news01">
+                  </div>
+                  <div class="col-md-8">
+                    <div class="card-body">
+                      <a href=""><h5 class="card-title">Card title</a></h5>
+                      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                      <p class="card-text"><small class="text-muted">Updated: Nov/3/2021</small></p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+        `
+
+      let title = items[i].getElementsByTagName('title')[0]
+      let texto = items[i].getElementsByTagName('description')[0]
+	  let date = items[i].getElementsByTagName('pubDate')[0]
+
+      plantilla = plantilla.replace('Card title', title.innerHTML)
+      plantilla = plantilla.replace('This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.', texto.innerHTML)
+	  plantilla = plantilla.replace('Updated: Nov/3/2021', date.innerHTML)
+
+      document.getElementsByClassName('noticias container')[0].innerHTML += plantilla
+     }
+
+     console.log(xml);
+   })
+
+
+   .catch(console.error)
+ }
+
+ peticion()
